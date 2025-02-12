930f7ad11aa2f8e415a9ba231af4dcdb91e28811
Gudelj22 <petargudelj96@gmail.com>
2025-02-12 12:30:12 +0100

    ja

--- Backend/LOLtrgovina.sql
+++ Backend/LOLtrgovina.sql
@@ -10,7 +10,7 @@ create table kupci(
 sifra int not null primary key identity (1,1),
 ime varchar(50) not null,
 prezime varchar(50) not null,
-datum_rodenja date not null,
+godine int not null,
 oib char(11) null
 );
 
@@ -38,8 +38,8 @@ narudzba int not null references  narudzbe(sifra),
 brojnarudzbe int not null
 );
 
-insert into kupci(ime, prezime, datum_rodenja) values
-('petar','gudelj','13-8-2005');
+insert into kupci(ime, prezime, godine) values
+('petar','gudelj','19');
 
 insert into robe(ime, vrsta, cijena, kolicina, kupac) values
 ('jhin','na daljinu','1500','1',1);
---
+++ Backend/Pocetak.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI9.Models;
+
+namespace WebAPI9.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")] // Ruta ce se zvati /api/v1/HttpMetode
+    public class HttpMetodeController : ControllerBase
+    {
+
+
+
+
+        
+        [HttpGet]
+        [Route("pozdrav")]
+        public string HelloWorld(string kupcu)
+        {
+            return $"Pozdrav {kupcu}!";
+        }
+
+
+       
+
+
+
+       
+        [HttpGet]
+        [Route("json")]
+        public IActionResult GetJson(string ime, string prezime, int godina, )
+        {
+            return Ok(new { Ime = ime, Prezime = prezime, godina = godina });
+        }
+
+
+       
+
+
+
+
+
+       
+
+        [HttpPost]
+        public IActionResult Post(Osoba osoba)
+        {
+            osoba.Ime = "Hello " + osoba.Ime;
+            return StatusCode(StatusCodes.Status201Created, osoba); // StatusCode(201, osoba);
+        }
+
+
+        
+
+
+   
+
+        [HttpPut]
+        public IActionResult Put(Osoba osoba)
+        {
+            osoba.Ime = "Promjenio " + osoba.Ime;
+            return Ok(osoba);
+        }
+
+
+      
+
+
+
+       
+
+        [HttpDelete]
+        public IActionResult Delete(int sifra)
+        {
+            if (sifra <= 0)
+            {
+                return BadRequest(new { poruka = "Sifra mora biti veca od 0" });
+            }
+            else
+            {
+                return Ok(new { poruka = "Obrisano" });
+
+            }
+        }
+        
+
+
+
+    }
+}
