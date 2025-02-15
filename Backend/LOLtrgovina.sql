663ab4ed27e1c286ba493e697503e890a4959f8e
pinti88 <tin.pinta@gmail.com>
2025-02-15 12:26:11 +0100

    jj

--- Backend/LOLtrgovina.sql
+++ Backend/LOLtrgovina.sql
@@ -1,10 +1,14 @@
-use master;
-go
-drop database if exists LOLtrgovina;
-go
-create database LOLtrgovina collate Croatian_CI_AS;
-go
-use LOLtrgovina;
+SELECT name, collation_name FROM sys.databases;
+GO
+ALTER DATABASE db_ab2ce7_loltrgovina SET SINGLE_USER WITH
+ROLLBACK IMMEDIATE;
+GO
+ALTER DATABASE db_ab2ce7_loltrgovina COLLATE Croatian_CI_AS;
+GO
+ALTER DATABASE db_ab2ce7_loltrgovina SET MULTI_USER;
+GO
+SELECT name, collation_name FROM sys.databases;
+GO
 
 create table kupci(
 sifra int not null primary key identity (1,1),
