adf522815d64fa96ac21ff4be99334ce88bd33a5
pinti88 <tin.pinta@gmail.com>
2025-03-08 12:27:35 +0100

    Update Pocetna.jsx

--- frontend/src/pages/Pocetna.jsx
+++ frontend/src/pages/Pocetna.jsx
@@ -4,11 +4,20 @@ import slika from '../assets/LoL.jpg'
 // https://getbootstrap.com/docs/4.0/utilities/display/
 
 export default function Pocetna(){
-    return(
-        <>
-        Dobro došli na moju aplikaciju 
-        <img src={slika} alt="LOL" className='.d-none .d-xl-block slika'/>
-        <img src={slikamobile} alt="LOL" className='.d-none .d-sm-block .d-md-none slika'/>
-        </>
-    )
-}
+    for (device);
+        {
+    if (computer)
+    {
+    <img src={slika} alt="LOL" className='.d-none .d-xl-block slika'/>
+    }
+    else (mobile){
+    <img src={slikamobile} alt="LOL" className='.d-none .d-sm-block  slika'/>
+    }
+    }
+    
+    
+
+
+
+    
+ 
