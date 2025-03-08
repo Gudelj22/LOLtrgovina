8b2763dcc98ff82e42c7f09a091b478c39449640
edunovaProgramiranje <84087934+edunovaProgramiranje@users.noreply.github.com>
2025-03-08 12:41:02 +0100

    FIX

--- frontend/src/pages/Pocetna.jsx
+++ frontend/src/pages/Pocetna.jsx
@@ -3,19 +3,17 @@ import slika from '../assets/LoL.jpg'
 
 // https://getbootstrap.com/docs/4.0/utilities/display/
 
+
+
 export default function Pocetna(){
-    for (device);
-        {
-    if (computer)
-    {
-    <img src={slika} alt="LOL" className='.d-none .d-xl-block slika'/>
-    }
-    else (mobile){
-    <img src={slikamobile} alt="LOL" className='.d-none .d-sm-block  slika'/>
-    }
-    }
+    return(
+    <>
+        <img src={slika} alt="LOL" className='d-none d-md-block d-lg-block d-xl-block w-100'/>
+        <img src={slikamobile} alt="LOL" className='d-block d-md-none d-lg-none w-100'/>
+    </>
+    )
     
-    
+}
 
 
 
